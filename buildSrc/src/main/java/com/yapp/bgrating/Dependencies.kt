e4c69062db32426eb074d6e27544a5a63619bc4c
EunJung Jung <agjjung@naver.com>
2023-05-19 06:22:27 +0900

    feat: Hilt, Retrofit, OkHttp dependency 추가를 위한 const val 선언

--- buildSrc/src/main/java/com/yapp/bgrating/Dependencies.kt
+++ buildSrc/src/main/java/com/yapp/bgrating/Dependencies.kt
@@ -24,6 +24,24 @@ object AndroidX {
     const val APP_COMPAT = "androidx.appcompat:appcompat:1.5.1"
 }
 
+object DaggerHilt {
+    const val DAGGER_HILT = "com.google.dagger:hilt-android:2.38.1"
+    const val DAGGER_HILT_COMPILER = "com.google.dagger:hilt-android-compiler:2.38.1"
+    const val DAGGER_HILT_VIEW_MODEL = "androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03"
+    const val DAGGER_HILT_ANDROIDX_COMPILER = "androidx.hilt:hilt-compiler:1.0.0"
+}
+
+object Retrofit {
+    const val RETROFIT = "com.squareup.retrofit2:retrofit:2.9.0"
+    const val CONVERTER_GSON = "com.squareup.retrofit2:converter-gson:2.9.0"
+    const val CONVERTER_JAXB = "com.squareup.retrofit2:converter-jaxb:2.9.0"
+}
+
+object OkHttp {
+    const val OKHTTP = "com.squareup.okhttp3:okhttp:4.9.1"
+    const val LOGGING_INTERCEPTOR = "com.squareup.okhttp3:logging-interceptor:4.9.1"
+}
+
 object KtLint {
     const val KTLINT = "org.jlleitschuh.gradle:ktlint-gradle:$KTLINT_VERSION"
 }

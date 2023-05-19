74534f34cfb3c61c5da327352099c253576e20e7
EunJung Jung <agjjung@naver.com>
2023-05-20 03:06:29 +0900

    feat: api post로 수정

--- data/src/main/java/com/yapp/bgrating/data/remote/OAuthApi.kt
+++ data/src/main/java/com/yapp/bgrating/data/remote/OAuthApi.kt
@@ -2,9 +2,13 @@ package com.yapp.bgrating.data.remote
 
 import com.yapp.bgrating.data.model.MockApiResponse
 import retrofit2.Response
+import retrofit2.http.Field
 import retrofit2.http.POST
 
 interface OAuthApi {
     @POST("auth/login")
-    suspend fun getMockApi(): Response<MockApiResponse>
+    suspend fun postMockApi(
+        @Field("type") type: String,
+        @Field("token") token: String,
+    ): Response<MockApiResponse>
 }

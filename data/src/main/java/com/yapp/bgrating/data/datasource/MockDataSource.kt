9e473e96313e95e9ab308d5bc964936452e59a07
EunJung Jung <agjjung@naver.com>
2023-05-19 23:14:09 +0900

    feat: MockDataSource 함수명, param 변경

--- data/src/main/java/com/yapp/bgrating/data/datasource/MockDataSource.kt
+++ data/src/main/java/com/yapp/bgrating/data/datasource/MockDataSource.kt
@@ -3,5 +3,5 @@ package com.yapp.bgrating.data.datasource
 import com.yapp.bgrating.data.model.MockApiResponse
 
 interface MockDataSource {
-    suspend fun getMock(mString: String): MockApiResponse?
+    suspend fun getKakaoMock(token: String): MockApiResponse?
 }
--- data/src/main/java/com/yapp/bgrating/data/datasource/impl/MockDataSourceImpl.kt
+++ data/src/main/java/com/yapp/bgrating/data/datasource/impl/MockDataSourceImpl.kt
@@ -5,7 +5,7 @@ import com.yapp.bgrating.data.model.MockApiResponse
 import javax.inject.Inject
 
 class MockDataSourceImpl @Inject constructor() : MockDataSource {
-    override suspend fun getMock(mString: String): MockApiResponse? {
+    override suspend fun getKakaoMock(token: String): MockApiResponse? {
         TODO("Not yet implemented")
     }
 }

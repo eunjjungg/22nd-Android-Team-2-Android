f61c8667a11622aae9d5ea1ff190b86cc521e26a
EunJung Jung <agjjung@naver.com>
2023-05-20 02:35:55 +0900

    feat: DataSourceImplModule 구현

--- data/src/main/java/com/yapp/bgrating/data/datasource/impl/MockDataSourceImpl.kt
+++ data/src/main/java/com/yapp/bgrating/data/datasource/impl/MockDataSourceImpl.kt
@@ -2,9 +2,12 @@ package com.yapp.bgrating.data.datasource.impl
 
 import com.yapp.bgrating.data.datasource.MockDataSource
 import com.yapp.bgrating.data.model.MockApiResponse
+import com.yapp.bgrating.data.remote.OAuthApi
 import javax.inject.Inject
 
-class MockDataSourceImpl @Inject constructor() : MockDataSource {
+class MockDataSourceImpl @Inject constructor(
+    private val oauthApi: OAuthApi
+) : MockDataSource {
     override suspend fun getKakaoMock(token: String): MockApiResponse? {
         TODO("Not yet implemented")
     }

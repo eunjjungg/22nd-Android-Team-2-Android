3c3cbcca0dba78662bc5f981f20a52f40a530720
EunJung Jung <agjjung@naver.com>
2023-05-20 03:17:10 +0900

    feat: DataSource BaseRepository 사용해서 내부 구현

--- data/src/main/java/com/yapp/bgrating/data/datasource/MockDataSource.kt
+++ data/src/main/java/com/yapp/bgrating/data/datasource/MockDataSource.kt
@@ -1,7 +1,8 @@
 package com.yapp.bgrating.data.datasource
 
 import com.yapp.bgrating.data.model.MockApiResponse
+import com.yapp.bgrating.domain.utils.RemoteErrorEmitter
 
 interface MockDataSource {
-    suspend fun getKakaoMock(token: String): MockApiResponse?
+    suspend fun getKakaoMock(remoteErrorEmitter: RemoteErrorEmitter, token: String): MockApiResponse?
 }
--- data/src/main/java/com/yapp/bgrating/data/datasource/impl/MockDataSourceImpl.kt
+++ data/src/main/java/com/yapp/bgrating/data/datasource/impl/MockDataSourceImpl.kt
@@ -3,12 +3,16 @@ package com.yapp.bgrating.data.datasource.impl
 import com.yapp.bgrating.data.datasource.MockDataSource
 import com.yapp.bgrating.data.model.MockApiResponse
 import com.yapp.bgrating.data.remote.OAuthApi
+import com.yapp.bgrating.data.utils.BaseRepository
+import com.yapp.bgrating.domain.utils.RemoteErrorEmitter
 import javax.inject.Inject
 
 class MockDataSourceImpl @Inject constructor(
     private val oauthApi: OAuthApi
-) : MockDataSource {
-    override suspend fun getKakaoMock(token: String): MockApiResponse? {
-        TODO("Not yet implemented")
+) : BaseRepository(), MockDataSource {
+    override suspend fun getKakaoMock(remoteErrorEmitter: RemoteErrorEmitter, token: String): MockApiResponse? {
+        return safeApiCall(remoteErrorEmitter) {
+            oauthApi.postMockApi("kakao", token).body()
+        }
     }
 }

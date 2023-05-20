30ae33f867d7f9742db3af35504da67806349771
EunJung Jung <agjjung@naver.com>
2023-05-20 15:19:43 +0900

    feat: MockApi request type을 @body와 data class로 변경

--- data/src/main/java/com/yapp/bgrating/data/datasource/impl/MockDataSourceImpl.kt
+++ data/src/main/java/com/yapp/bgrating/data/datasource/impl/MockDataSourceImpl.kt
@@ -1,7 +1,9 @@
 package com.yapp.bgrating.data.datasource.impl
 
 import com.yapp.bgrating.data.datasource.MockDataSource
+import com.yapp.bgrating.data.model.MockApiRequest
 import com.yapp.bgrating.data.model.MockApiResponse
+import com.yapp.bgrating.data.model.MockType
 import com.yapp.bgrating.data.remote.OAuthApi
 import com.yapp.bgrating.data.utils.BaseRepository
 import com.yapp.bgrating.domain.utils.RemoteErrorEmitter
@@ -12,7 +14,7 @@ class MockDataSourceImpl @Inject constructor(
 ) : BaseRepository(), MockDataSource {
     override suspend fun getKakaoMock(remoteErrorEmitter: RemoteErrorEmitter, token: String): MockApiResponse? {
         return safeApiCall(remoteErrorEmitter) {
-            oauthApi.postMockApi("kakao", token).body()
+            oauthApi.postMockApi(MockApiRequest(MockType.KAKAO, "SUCCESSid")).body()
         }
     }
 }
--- data/src/main/java/com/yapp/bgrating/data/remote/OAuthApi.kt
+++ data/src/main/java/com/yapp/bgrating/data/remote/OAuthApi.kt
@@ -1,14 +1,14 @@
 package com.yapp.bgrating.data.remote
 
+import com.yapp.bgrating.data.model.MockApiRequest
 import com.yapp.bgrating.data.model.MockApiResponse
 import retrofit2.Response
-import retrofit2.http.Field
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface OAuthApi {
     @POST("auth/login")
     suspend fun postMockApi(
-        @Field("type") type: String,
-        @Field("token") token: String,
+        @Body mockApiRequest: MockApiRequest,
     ): Response<MockApiResponse>
 }

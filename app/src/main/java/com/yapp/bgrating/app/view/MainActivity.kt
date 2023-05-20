2e400434f633bba84a2563552674a7affd504441
EunJung Jung <agjjung@naver.com>
2023-05-20 15:19:59 +0900

    feat: MockApi test용 샘플 코드 구현

--- app/src/main/java/com/yapp/bgrating/app/view/MainActivity.kt
+++ app/src/main/java/com/yapp/bgrating/app/view/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.yapp.bgrating.app.view
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.yapp.bgrating.app.R
+import com.yapp.bgrating.app.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val mainViewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainViewModel.loginTest()
     }
 }
